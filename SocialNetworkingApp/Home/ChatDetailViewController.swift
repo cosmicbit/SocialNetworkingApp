5c2d673bc26a205f64d8a7c7232c8069895c859f
Philips <philipsjose2003@gmail.com>
2025-07-28 18:25:39 +0530

    message bar animation when keyborad is present completed

--- SocialNetworkingApp/Home/ChatDetailViewController.swift
+++ SocialNetworkingApp/Home/ChatDetailViewController.swift
@@ -15,6 +15,7 @@ class ChatDetailViewController: UIViewController {
     
     private var tableView: UITableView!
     private var messageBar: MessageBarView!
+    private var messageBarBottomConstraint: NSLayoutConstraint!
     
     var otherUserProfile: UserProfile!
     
@@ -22,10 +23,15 @@ class ChatDetailViewController: UIViewController {
         super.viewDidLoad()
         
         setupView()
-        
+        setupKeyboardObservers()
         
         
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
     }
     
     func setupView(){
@@ -36,16 +42,16 @@ class ChatDetailViewController: UIViewController {
         messageBar = MessageBarView()
         messageBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(messageBar)
-        
+        messageBarBottomConstraint = messageBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: backButton.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: messageBar.topAnchor),
             messageBar.heightAnchor.constraint(equalToConstant: 50),
-            messageBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
             messageBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
-            messageBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5)
+            messageBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+            messageBarBottomConstraint
         ])
     }
     
@@ -67,6 +73,56 @@ class ChatDetailViewController: UIViewController {
         subtitleAttributes.foregroundColor = .lightGray
         config.attributedSubtitle = AttributedString(config.subtitle!, attributes: subtitleAttributes)
         otherUserNameButton.configuration = config
+    }
+    
+    func setupKeyboardObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+
+    // Call this method typically from viewWillDisappear(_:) or deinit
+    func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+              let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+              let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {
+            return
+        }
+
+        let safeAreaBottomInset = view.safeAreaInsets.bottom // Account for safe area
+        let keyboardHeight = keyboardFrame.height
+        
+        messageBarBottomConstraint.constant = -(keyboardHeight - safeAreaBottomInset)
+        UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+            self.view.layoutIfNeeded() // Animate the layout change
+        }, completion: nil)
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+              let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {
+            return
+        }
+
+        messageBarBottomConstraint.constant = -10
+
+        UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+            self.view.layoutIfNeeded() // Animate the layout change
+        }, completion: nil)
     }
     
 
